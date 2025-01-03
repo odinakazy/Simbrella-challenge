eed3a6b5cdbf02c2e0d2165c122a2f64172a7b72
odinakazy <odinakasolomon4@gmail.com>
2025-01-03 08:46:34 -0700

    Feat:readme file updated

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function App() {
+  return (
+    <div className="flex justify-center items-center">
+      <Button>Click me</Button>
+      <h2 className="text-red-500">hello </h2>
+    </div>
+  );
+}
