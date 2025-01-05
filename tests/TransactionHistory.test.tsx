bed858dc4346e3bbc823284cf6236f7b06940fd9
odinakazy <odinakasolomon4@gmail.com>
2025-01-05 16:37:12 -0700

    fix-bugs

--- tests/TransactionHistory.test.tsx
+++ tests/TransactionHistory.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import TransactionHistory from "../src/dashboard/Transaction/transaction";
-import React from "react";
+// import React from "react";
 import "@testing-library/jest-dom";
 
 // Mock data for testing
